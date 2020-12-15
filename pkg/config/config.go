45c6bedf783e83503a79c49e16411b73ef49b70c
stevehebert <stevehebert@shipt.com>
2020-12-15 10:36:49 -0600

    made CreateDefinition more unique to prevent naming collisions

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,8 +5,8 @@ import (
 	"github.com/shipt/plinko/internal/runtime"
 )
 
-// CreateDefinition creates a new structure used in defining the state machine.
-func CreateDefinition() plinko.PlinkoDefinition {
+// CreatePlinkoDefinition ... creates a new structure used in defining the state machine.
+func CreatePlinkoDefinition() plinko.PlinkoDefinition {
 	stateMap := make(map[plinko.State]*runtime.InternalStateDefinition)
 	p := runtime.PlinkoDefinition{
 		States: &stateMap,
