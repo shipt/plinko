324757231156ebe729013060d1fce18042fdf04e
stevehebert <stevehebert@shipt.com>
2021-07-20 11:45:18 -0500

    fix test

--- internal/runtime/internal_test.go
+++ internal/runtime/internal_test.go
@@ -21,7 +21,7 @@ func TestGetCallerHelper(t *testing.T) {
 	})
 
 	// this follow test is based on the location of the above line
-	assert.Equal(t, "internal/runtime/internal_test.go:17", n)
+	assert.Equal(t, "internal/runtime/internal_test.go:19", n)
 
 }
 
